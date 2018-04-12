605c9860326cd9438dc57605162007405b089ae1
crhntr <hunter.christopher@icloud.com>
2018-04-12 00:14:28 -0700

    Inital Download Script

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+)
+
+var (
+	outPath = *flag.String("out", "data", "Output Folder for Downloads")
+)
+
+func main() {
+	flag.Parse()
+
+	f, err := os.Open("downloads.json") // https://api.fda.gov/download.json
+	if err != nil {
+		panic(err)
+	}
+	var downlaods Downloads
+	if err = json.NewDecoder(f).Decode(&downlaods); err != nil {
+		panic(err)
+	}
+
+	switch flag.Arg(0) {
+	case "drug.event":
+		switch flag.Arg(1) {
+		case "size":
+			fmt.Println(downlaods.Results.Drug.Event.Size())
+		case "download":
+			size := downlaods.Results.Drug.Event.Size()
+			var downlaoded float64
+
+			for i, part := range downlaods.Results.Drug.Event.Partitions {
+				func(i int, part Partition) {
+					log.Printf("%d %q %.2f %.2f", i, part.Name, part.Size, (downlaoded/size)*100)
+					resp, err := http.Get(part.File)
+					if err != nil {
+						log.Printf("%d %s %s", i, part.File, err)
+						return
+					}
+					defer resp.Body.Close()
+
+					out, err := os.Create(path.Join(outPath, path.Base(part.File)))
+					if err != nil {
+						log.Printf("%d %s %s", i, part.File, err)
+						return
+					}
+					defer out.Close()
+					io.Copy(out, resp.Body)
+					downlaoded += part.Size
+				}(i, part)
+			}
+		}
+	}
+}

0f8e35e1738bb465a9c80e4956926f0f0407994e
crhntr <hunter.christopher@icloud.com>
2018-04-12 22:49:50 -0700

    Fixed Name Collisions On Download

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strings"
 )
 
 var (
@@ -32,12 +33,23 @@ func main() {
 		switch flag.Arg(1) {
 		case "size":
 			fmt.Println(downlaods.Results.Drug.Event.Size())
+		case "parse":
+			// for i, file := range flag.Args()[2:] {
+			//
+			// }
 		case "download":
 			size := downlaods.Results.Drug.Event.Size()
 			var downlaoded float64
 
 			for i, part := range downlaods.Results.Drug.Event.Partitions {
 				func(i int, part Partition) {
+					dir := path.Dir(strings.TrimPrefix(part.File, "https://download.open.fda.gov/"))
+
+					if err := os.MkdirAll(path.Join(outPath, dir), os.ModePerm); err != nil {
+						log.Printf("%d %s %s", i, part.File, err)
+						return
+					}
+
 					log.Printf("%d %q %.2f %.2f", i, part.Name, part.Size, (downlaoded/size)*100)
 					resp, err := http.Get(part.File)
 					if err != nil {
@@ -46,7 +58,7 @@ func main() {
 					}
 					defer resp.Body.Close()
 
-					out, err := os.Create(path.Join(outPath, path.Base(part.File)))
+					out, err := os.Create(path.Join(outPath, dir, path.Base(part.File)))
 					if err != nil {
 						log.Printf("%d %s %s", i, part.File, err)
 						return

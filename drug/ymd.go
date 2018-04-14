ea152a25e40720e67c5b3596a299e463a8cfea19
crhntr <hunter.christopher@icloud.com>
2018-04-13 21:58:58 -0700

    Reorg

---
+++ drug/ymd.go
@@ -0,0 +1,32 @@
+package drug
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+)
+
+type YearMonthDay struct {
+	time.Time
+}
+
+func (ct *YearMonthDay) UnmarshalJSON(b []byte) (err error) {
+	b = bytes.Trim(b, "\"")
+	ct.Time, err = time.Parse("20060102", string(b))
+	if err == nil {
+		return nil
+	}
+	ct.Time, err = time.Parse("200601", string(b))
+	if err == nil {
+		return nil
+	}
+	ct.Time, err = time.Parse("2006", string(b))
+	if err == nil {
+		return nil
+	}
+	return err
+}
+
+func (ct *YearMonthDay) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("%04d%02d%02d", ct.Year(), ct.Month(), ct.Day())), nil
+}

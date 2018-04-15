0f2b3bd19766c84024248abb72a9f6d880c6e65c
crhntr <hunter.christopher@icloud.com>
2018-04-15 03:19:13 -0700

    Moved Downloads File

--- download.go
+++ download.go
@@ -48,7 +48,7 @@ func download() {
 }
 
 func OpenDownloads() Downloads {
-	f, err := os.Open("downloads.json") // https://api.fda.gov/download.json
+	f, err := os.Open("data/downloads.json") // https://api.fda.gov/download.json
 	if err != nil {
 		panic(err)
 	}

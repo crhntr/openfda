bcd16986e2dee43223f0f8ce4215df5b5ab20e71
crhntr <hunter.christopher@icloud.com>
2018-04-15 03:18:13 -0700

    Fixed Index Sort Order

--- dbIndexes.js
+++ dbIndexes.js
@@ -1,7 +1,1 @@
-
-
-db.drug_event.createIndex({
-  'sex': 1,
-  's': 1,
-  'drugs.openfda._id'
-}, {'sparse': true})
+db.drug_event.createIndex({'sex': 1,'s': 1,'drugs.openfda._id': 1}, {'sparse': true})

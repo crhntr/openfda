debffc87885880424b612ae5496280f5f5b2c56f
crhntr <hunter.christopher@icloud.com>
2018-05-16 01:26:47 -0700

    Added Some Nice Whitespace

--- cmd/openfda/main.go
+++ cmd/openfda/main.go
@@ -8,26 +8,40 @@ import (
 
 func main() {
 	flag.Parse()
+
 	switch flag.Arg(0) {
 	default:
+
 		fmt.Println("unknown argument")
+
 	case "downloads-file":
+
 		downloadsFile()
+
 	case "drug":
+
 		switch flag.Arg(1) {
+
 		case "event":
+
 			switch flag.Arg(2) {
+
 			case "size":
 				downloads := openDownloads()
 				fmt.Println(downloads.Results.Drug.Event.Partitions.Filter(*year, *quarter).Size())
+
 			case "import":
 				importFiles()
+
 			case "download":
 				download()
+
 			case "import-all":
 				importAll()
+
 			case "count":
 				countDrugEvents()
+
 			case "count-files":
 				var count int
 				eachDrugEventFile(func(f *os.File) error {
@@ -35,12 +49,15 @@ func main() {
 					return nil
 				})
 				fmt.Printf("\n\nfound %d files\n\n", count)
+
 			case "ls":
 				eachDrugEventFile(func(f *os.File) error {
 					fmt.Println(f.Name())
 					return nil
 				})
+
 			}
+
 		case "label":
 
 		}

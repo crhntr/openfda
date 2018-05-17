afb36eee2bac7f1370b50438aa8a71d1db3c50b5
crhntr <hunter.christopher@icloud.com>
2018-05-16 23:31:19 -0700

    Changed underlying type

--- drug/ymd.go
+++ drug/ymd.go
@@ -2,31 +2,28 @@ package drug
 
 import (
 	"bytes"
-	"fmt"
 	"time"
 )
 
-type YearMonthDay struct {
-	time.Time
-}
+type YearMonthDay string
 
 func (ct *YearMonthDay) UnmarshalJSON(b []byte) (err error) {
 	b = bytes.Trim(b, "\"")
-	ct.Time, err = time.Parse("20060102", string(b))
+
+	(*ct) = YearMonthDay(string(b))
+
+	_, err = time.Parse("20060102", string(b))
 	if err == nil {
 		return nil
 	}
-	ct.Time, err = time.Parse("200601", string(b))
+	_, err = time.Parse("200601", string(b))
 	if err == nil {
 		return nil
 	}
-	ct.Time, err = time.Parse("2006", string(b))
-	if err == nil {
-		return nil
-	}
+	_, err = time.Parse("2006", string(b))
 	return err
 }
 
-func (ct *YearMonthDay) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf("\"%04d%02d%02d\"", ct.Year(), ct.Month(), ct.Day())), nil
+func (ct YearMonthDay) MarshalJSON() ([]byte, error) {
+	return []byte(ct), nil
 }
